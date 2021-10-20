c9b40ca8750ce149ddc7bb8a15c4ce76019efbc5
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-10-20 09:34:01 -0700

    Update library.py

--- library.py
+++ library.py
@@ -2,3 +2,92 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+'''
+Chapter 3 asks for chapter 2 transformers to be placed here
+titanic_transformer = Pipeline(steps=[
+    ('drop', up.DropColumnsTransformer(['Age', 'Gender', 'Class', 'Joined', 'Married',  'Fare', 'Survived'], 'keep')),
+    ('gender', up.MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('class', up.MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('ohe', up.OHETransformer(target_column='Joined')),
+    ], verbose=True)
+'''
+
+
+#drop by removing or keeping
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'DropColumnsTransformer action {action} not in ["keep", "drop"]'
+    self.column_list = column_list
+    self.action = action
+    
+  #fill in rest below
+  def fit(self, X, y = None):
+    print("Warning: DropColumnsTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'DropColumnsTransformer.transform expected Dataframe but got {type(X)} instead.'
+    
+    X_ = X.copy()
+    xcols = X.columns.to_list()
+    actual_list = [col for col in self.column_list if col in xcols]  #some might be missing from X columns
+    if self.action=='drop':
+      if actual_list != self.column_list:
+        print(f'DropColumnsTransformer.transform warning: columns to drop not in X: {set(self.column_list) - set(xcols)}')
+      X_ = X_.drop(columns=actual_list)
+    else:
+      if actual_list != self.column_list:
+        print(f'DropColumnsTransformer.transform warning: columns to keep not in X: {set(self.column_list) - set(xcols)}')
+      X_ = X_[actual_list]
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+#This class maps values in a column, numeric or categorical.
+#Importantly, it does not change NaNs, leaving that for the imputer step.
+class MappingTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, mapping_column, mapping_dict:dict):  
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print("Warning: MappingTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MappingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+    
+
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=True):  
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+ 
+  def fit(self, X, y = None):
+    print("Warning: OHETransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'OHETransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'OHETransformer.transform unknown column {self.target_column}'
+    X_ = X.copy()
+    X_ = pd.get_dummies(X_, columns=[self.target_column],
+                        dummy_na=self.dummy_na,
+                        drop_first = self.drop_first)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
